0ac1511bf841632d34ddd84250a02ec807c14a2b
Rafael Lins <rafael.lins@glovoapp.com>
2021-12-21 11:47:53 -0300

    [NO-TICKET] Test libs bump

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,10 +33,10 @@ repositories {
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 
-    testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
-    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.8.1")
-    testImplementation("io.kotest:kotest-assertions-core-jvm:5.0.0.RC")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.8.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.8.2")
+    testImplementation("io.kotest:kotest-assertions-core-jvm:5.0.2")
 }
 
 tasks {
