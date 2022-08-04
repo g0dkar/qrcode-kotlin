14cbb8577c74830cb870276f664209532713cb48
Rafael Lins <rafael.lins777@gmail.com>
2022-08-04 11:24:05 -0300

    Adding an example that generates an SVG QRCode

--- examples/kotlin/src/main/kotlin/SVGQRCode.kt
+++ examples/kotlin/src/main/kotlin/SVGQRCode.kt
@@ -2,14 +2,23 @@ import io.github.g0dkar.qrcode.QRCode
 import java.io.FileOutputStream
 
 class SVGQRCode {
+    companion object {
+        private const val USE_CSS = true
+
+        private val svgRenderer = SVGQRCodeGraphics(
+            QRCode.DEFAULT_CELL_SIZE,
+            QRCode.DEFAULT_CELL_SIZE,
+            USE_CSS
+        )
+    }
     fun createQRCode(content: String) {
-        FileOutputStream("kotlin-simple.png").use {
-            QRCode(content).render().writeImage(it)
+        FileOutputStream("kotlin-svg.svg").use {
+            QRCode(content).render(qrCodeGraphics = svgRenderer).writeImage(it)
         }
     }
 }
 
 fun main() {
-    SimpleQRCode()
+    SVGQRCode()
         .createQRCode("Hello, world!")
 }
--- examples/kotlin/src/main/kotlin/SVGQRCodeGraphics.kt
+++ examples/kotlin/src/main/kotlin/SVGQRCodeGraphics.kt
@@ -1,17 +1,30 @@
 import io.github.g0dkar.qrcode.render.QRCodeGraphics
+import java.awt.Graphics2D
+import java.io.OutputStream
+import java.io.OutputStreamWriter
 import org.apache.batik.dom.GenericDOMImplementation
 import org.apache.batik.svggen.SVGGraphics2D
-import java.awt.Graphics2D
 
-class SVGQRCodeGraphics(width: Int, height: Int) : QRCodeGraphics(width, height) {
+class SVGQRCodeGraphics(width: Int, height: Int, var useCSS: Boolean = true) :
+    QRCodeGraphics(width, height) {
     companion object {
         private const val SVG_NS = "http://www.w3.org/2000/svg"
         private const val SVG_QN = "svg"
+        private const val SVG_CHARSET = "UTF-8"
     }
 
     private val svg = GenericDOMImplementation
         .getDOMImplementation()
         .createDocument(SVG_NS, SVG_QN, null)
 
-    override fun createGraphics(): Graphics2D = SVGGraphics2D(svg)
+    private val svgGenerator = SVGGraphics2D(svg)
+
+    override fun createGraphics(): Graphics2D = svgGenerator
+
+    override fun nativeImage(): Any = svg
+
+    override fun writeImage(destination: OutputStream, format: String) {
+        val writer = OutputStreamWriter(destination, SVG_CHARSET)
+        svgGenerator.stream(writer, useCSS)
+    }
 }
