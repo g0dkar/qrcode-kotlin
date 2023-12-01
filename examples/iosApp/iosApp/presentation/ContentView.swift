bf675ef295339aae992ff099282121bf35acc556
Rui Canas <ruijfcanas@gmail.com>
2023-12-01 20:42:02 +0000

    Enhancing iOS sample app presentation

--- examples/iosApp/iosApp/presentation/ContentView.swift
+++ examples/iosApp/iosApp/presentation/ContentView.swift
@@ -17,24 +17,34 @@ struct ContentView: View {
         .withColor(color: Colors().BLACK)
         .withSize(size: 10)
         .withInnerSpacing(innerSpacing: 0)
-        .build(data: "https://github.com/g0dkar/qrcode-kotlin")
-        .renderToBytes(format: "PNG")
+        .build(data: Strings.data)
+        .renderToBytes(format: Strings.imageFormat)
 
     var body: some View {
-        VStack {
-            HStack {
-                Image(
-                    uiImage: UIImage(data: qrCode.toData) ?? .init()
-                )
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .frame(width: 200, height: 200)
-                .background(Color.clear)
-            }
+        VStack(spacing: 30) {
+            Text(Strings.title)
+                .font(.title2)
+                .foregroundColor(.blue)
+                .bold()
+            
+            Image(
+                uiImage: UIImage(data: qrCode.toData) ?? .init()
+            )
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .frame(width: 200, height: 200)
+            .background(Color.clear)
         }
     }
 }
 
 #Preview {
     ContentView()
+}
+
+private enum Strings {
+    
+    static let title = "QRCode-Kotlin"
+    static let data = "https://github.com/g0dkar/qrcode-kotlin"
+    static let imageFormat = "PNG"
 }
