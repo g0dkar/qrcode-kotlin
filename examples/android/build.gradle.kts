cd60b7a97324aaf9777196f41d80e36178ec7edb
Rafael Lins <rafael.araujo@swile.com.br>
2025-02-22 17:15:09 -0300

    feat(4.2.0): Example updates

--- examples/android/build.gradle.kts
+++ examples/android/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "io.github.g0dkar.qrcode"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "io.github.g0dkar.qrcodeKotlin"
