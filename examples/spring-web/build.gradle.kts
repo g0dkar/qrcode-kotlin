6683643bcfb198089624c37d621c6b9d18fb767b
Rafael Lins <rafael.araujo@swile.com.br>
2025-02-26 17:06:55 -0300

    feat(info-density): Docs and tests dependencies

--- examples/spring-web/build.gradle.kts
+++ examples/spring-web/build.gradle.kts
@@ -1,17 +1,17 @@
 plugins {
-	kotlin("jvm")
-	kotlin("plugin.spring") version "1.9.25"
-	id("org.springframework.boot") version "3.4.3"
-	id("io.spring.dependency-management") version "1.1.7"
+    kotlin("jvm")
+    kotlin("plugin.spring") version "1.9.25"
+    id("org.springframework.boot") version "3.4.3"
+    id("io.spring.dependency-management") version "1.1.7"
 }
 
 group = "io.github.g0dkar.qrcode"
 version = "1.0"
 
 java {
-	toolchain {
-		languageVersion = JavaLanguageVersion.of(17)
-	}
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(17)
+    }
 }
 
 repositories {
@@ -20,30 +20,30 @@ repositories {
 }
 
 dependencies {
-	implementation(kotlin("reflect"))
+    implementation(kotlin("reflect"))
 
     // Spring Web
-	implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-web")
 
     // Jackson Kotlin Support
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 
     // o11y
-	implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
 
     // QRCode-Kotlin
     implementation("io.github.g0dkar:qrcode-kotlin:4.3.0")
 
     // Tests
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    // testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
 kotlin {
-	compilerOptions {
-		freeCompilerArgs.addAll("-Xjsr305=strict")
-	}
+    compilerOptions {
+        freeCompilerArgs.addAll("-Xjsr305=strict")
+    }
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
+    useJUnitPlatform()
 }
